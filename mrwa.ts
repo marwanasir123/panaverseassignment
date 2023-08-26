eab6669d13bba6e1e3ef57f331b3568046872457
marwanasirgcw@gmail.com <marwanasiruaf@gmail.com>
2023-08-26 16:57:09 +0500

    assignment 1

---
+++ mrwa.ts
@@ -0,0 +1,24 @@
+var name1:string="'Hey Marwa Nair,i have started learning TYPESCRIPT'";
+console.log(name1);
+var num1:number=55;
+var num2:number=59;
+var num3:number=53;
+var num4:number=25;
+var num5:number=89;
+var num6:number=13;
+var num7:number=10;
+var num8:number=12;
+var num9:number=18;
+var num10:number=62;
+var add:number=num1+num2+num3+num4+num5+num6+num7+num8+num9+num10;
+console.log("Addition"+' '+add);
+var subtraction:number=num1-num2-num3-num4-num5-num6-num7-num8-num9-num10;
+console.log("subtraction"+' '+subtraction);
+var multi:number=num1*num2*num3*num4*num5*num6 *num7*num8*num9*num10;
+console.log("multiplying"+' '+multi);
+var division:number=num10/num5;
+console.log("division"+' '+division);
+var pio:number=num2+(num3*num6)/num4-num9;
+console.log(pio);
+
+
